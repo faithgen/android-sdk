3dd9c1a8195a02855c8e152ad825833d5a1fa2be
innoflash <innocentmazando@gmail.com>
2020-02-20 20:16:04 +0200

    documented  listeners

--- sdk/src/main/java/net/faithgen/sdk/interfaces/ServerResponseListener.kt
+++ sdk/src/main/java/net/faithgen/sdk/interfaces/ServerResponseListener.kt
@@ -2,7 +2,21 @@ package net.faithgen.sdk.interfaces
 
 import net.faithgen.sdk.http.ErrorResponse
 
+/**
+ * Listens to HTTP events
+ */
 interface ServerResponseListener {
+    /**
+     * Handles the response on a successful request
+     *
+     * @param serverResponse The response from the server
+     */
     fun onResponse(serverResponse: String?)
+
+    /**
+     * Handles the response on a failed request
+     *
+     * @param errorResponse The manufactured error response from Volley
+     */
     fun onError(errorResponse: ErrorResponse?)
 }
--- sdk/src/main/java/net/faithgen/sdk/interfaces/UserAuthListener.kt
+++ sdk/src/main/java/net/faithgen/sdk/interfaces/UserAuthListener.kt
@@ -1,5 +1,11 @@
 package net.faithgen.sdk.interfaces
 
+/**
+ * Listens to user sign in
+ */
 interface UserAuthListener {
+    /**
+     * The action to take when "sign in" is tapped
+     */
     fun onSignInTapped()
 }
