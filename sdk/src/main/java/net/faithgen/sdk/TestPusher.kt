820a821ddb203c24e19ca70fa2aa07c17a1f46a7
innoflash <innocentmazando@gmail.com>
2020-04-05 16:24:42 +0200

    connected to websocket

--- sdk/src/main/java/net/faithgen/sdk/TestPusher.kt
+++ sdk/src/main/java/net/faithgen/sdk/TestPusher.kt
@@ -1,8 +1,8 @@
 package net.faithgen.sdk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.pusher.client.Pusher
 import com.pusher.client.PusherOptions
 import com.pusher.client.connection.ConnectionEventListener
@@ -10,20 +10,20 @@ import com.pusher.client.connection.ConnectionState
 import com.pusher.client.connection.ConnectionStateChange
 import com.pusher.client.util.HttpAuthorizer
 import kotlinx.android.synthetic.main.activity_test_pusher.*
-import java.lang.Exception
 
 class TestPusher : AppCompatActivity() {
 
     private val authorizer: HttpAuthorizer by lazy { HttpAuthorizer("http://192.168.8.101:8001/laravel-websockets/auth") }
 
-    private val pusherOptions : PusherOptions by lazy {
+    private val pusherOptions: PusherOptions by lazy {
         authorizer.setHeaders(mapOf(Pair("X-App-ID", "myId")))
 
         PusherOptions()
-                .setHost("http://192.168.8.101")
+                .setHost("192.168.8.101")
                 .setWsPort(6001)
                 .setWssPort(6001)
                 .setAuthorizer(authorizer)
+                .setEncrypted(false)
     }
 
     private val pusher: Pusher by lazy { Pusher("myKey", pusherOptions) }
@@ -42,12 +42,13 @@ class TestPusher : AppCompatActivity() {
             }
 
             override fun onError(message: String?, code: String?, e: Exception?) {
-               updateStatus(message!!, "Error")
+                e!!.printStackTrace()
+                updateStatus(message!!, "Error")
             }
         }, ConnectionState.ALL)
     }
 
-    private fun updateStatus(message: String, operation : String = "change"){
+    private fun updateStatus(message: String, operation: String = "change") {
         statusText.text = message
         Log.d(operation, message)
     }
